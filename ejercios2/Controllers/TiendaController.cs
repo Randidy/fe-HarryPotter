f9e1099900cb98dcc8d19a455959e386078c0635
Randidy <andrearebatta0623@gmail.com>
2025-04-01 00:29:20 -0500

    primer commit

---
+++ ejercios2/Controllers/TiendaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ejercios2.Controllers
+{
+    public class TiendaController : Controller
+    {
+        // GET: Tienda
+        public ActionResult Index()
+        {
+            return View();
+        }
+   
+        public ActionResult Casas()
+        {
+            
+            return View();
+        }
+        public ActionResult Criaturas()
+        {
+            return View();
+        }
+        public ActionResult Hechizos()
+        {
+            return View();
+        }
+
+        public ActionResult Actores()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ejercios2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ejercios2.MvcApplication" Language="C#" %>
